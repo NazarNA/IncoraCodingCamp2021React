f67743f6f83d98cae8a41f3ee771aec5d9fd2d38
Nazar <nazarnovosad610@gmail.com>
2021-03-01 01:26:47 +0200

    add react-roter and fix layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import './App.scss';
 
@@ -15,19 +16,14 @@ function App() {
         <section>
           <h1>Log In</h1>
           <p>
-            If you don`t have an account yet, please{' '}
-            <a href='https://www.gmail.com' target='_blank'>
-              register
-            </a>{' '}
+            If you don`t have an account yet, please <Link to='#'> register </Link>
             first.
           </p>
         </section>
         <form onSubmit={e => e.preventDefault()}>
           <input type='email' name='email' placeholder='Your e-mail' />
           <input type='password' name='password' placeholder='Your password' />
-          <a href='https://www.gmail.com' target='_blank'>
-            I forgot my password
-          </a>
+          <Link to='#'>I forgot my password</Link>
           <label htmlFor='remember_me'>
             <input type='radio' id='remember_me' value='Remember me' />
             &nbsp; Remember me
