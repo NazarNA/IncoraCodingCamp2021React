b1ab72248da82c3518f5888fe06e7a3d69a5c999
Nazar <nazarnovosad610@gmail.com>
2021-03-01 23:11:23 +0200

    create pagination

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,41 @@
 import React from 'react';
+import Pagination from './components/Pagination/Pagination';
 
+import styles from './components/Pagination/Pagination.module.scss';
 import './App.scss';
 
 function App() {
+  const [currentPage, setCurrentPage] = React.useState(1);
+
+  const onChangePageHandler = (page: number) => {
+    setCurrentPage(page);
+  };
+
+  const incrementPage = () => {
+    setCurrentPage(currentPage + 1);
+  };
+
+  const decrementPage = () => {
+    setCurrentPage(currentPage - 1);
+  };
+
   return (
-    <div className='wrapper'>
-      <aside>
-        <img
-          src='https://cdn.shortpixel.ai/client/q_glossy,ret_img,w_1024,h_768/https://globalhelpswap.com/wp-content/uploads/2019/09/Things-to-do-in-Trentino-24-1024x768.jpg'
-          alt='nature_icon'
-        />
-      </aside>
-      <main>
-        <section>
-          <h1>Log In</h1>
-          <p>
-            If you don`t have an account yet, please{' '}
-            <a href='https://www.gmail.com' target='_blank'>
-              register
-            </a>{' '}
-            first.
-          </p>
-        </section>
-        <form onSubmit={e => e.preventDefault()}>
-          <input type='email' name='email' placeholder='Your e-mail' />
-          <input type='password' name='password' placeholder='Your password' />
-          <a href='https://www.gmail.com' target='_blank'>
-            I forgot my password
-          </a>
-          <label htmlFor='remember_me'>
-            <input type='radio' id='remember_me' value='Remember me' />
-            &nbsp; Remember me
-          </label>
-          <input type='submit' value='Log in' className='form_submit' />
-        </form>
-      </main>
+    <div>
+      <Pagination
+        totalItems={50}
+        perPage={10}
+        withActions={true}
+        classes={{
+          btn: styles.btn,
+          paginationWrapper: styles.paginationWrapper,
+          activeBtn: styles.active,
+          pageView: styles.pageView
+        }}
+        activePage={currentPage}
+        onChangePage={onChangePageHandler}
+        incrementPage={incrementPage}
+        decrementPage={decrementPage}
+      />
     </div>
   );
 }
